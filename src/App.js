168e5d905bd5738fce936421e9e3d90ba4352708
MarkNewcomb1 <wlmmn@hotmail.com>
2018-02-12 14:31:03 -0700

    no uncontrolled components

--- src/App.js
+++ src/App.js
@@ -47,15 +47,17 @@ class App extends Component {
 		}
 	
   render() {
+	  const { searchTerm, list } = this.state;
     return (
       <div className="App">
 	      <form>
 		      <input 
 		      type="text" 
+		      value={searchTerm}
 		      onChange={this.onSearchChange}
 		      />
 	      </form>
-	  	{this.state.list.filter(isSearched(this.state.searchTerm)).map(item => {
+	  	{list.filter(isSearched(searchTerm)).map(item => {
 		  	const onHandleDismiss = () =>
 		  		this.onDismiss(item.objectID);
 		  		return (
